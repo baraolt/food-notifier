b7991bdc0cce8df4ab2c15336e45b8cc4c3506c0
Guillelerial <guillermo.gc1994@gmail.com>
2020-08-09 17:12:30 +0200

    Update Favourite food notifier WIN.py
    Refactoring and fixed missing ' on string

--- Favourite food notifier WIN.py
+++ Favourite food notifier WIN.py
@@ -1,38 +1,33 @@
 #! python3
-import bs4, requests, smtplib
+import bs4
+import requests
+import smtplib
 
-# ------------------- E-mail list ------------------------
-toAddress = ['example1@email.com','example2@email.com']
-# --------------------------------------------------------
+the_one = 'borzaska'  # This is the name of the food you are looking for
+to_address = ['example1@email.com', 'example2@email.com']  # E-mail list
+email_content = 'Subject: Borzaska Alert!\n\nAttention!\n\nYour favourite food is available today!' \
+                '\n\nBon apetite!:\nFood Notifier V1.0'
 
-#Download page
-getPage = requests.get('http://www.somesite.com/menu')
-getPage.raise_for_status() #if error it will stop the program
+if __name__ == '__main__':
+    # Download page
+    getPage = requests.get('http://www.somesite.com/menu')
+    getPage.raise_for_status()  # if error it will stop the program
 
-#Parse text for foods
-menu = bs4.BeautifulSoup(getPage.text, 'html.parser')
-foods = menu.select('.foodname')
+    # Parse text for foods
+    menu = bs4.BeautifulSoup(getPage.text, 'html.parser')
+    foods = menu.select('.foodname')
 
-the_one = 'borzaska' # This is the name of the food you are looking for
-flength = len(the_one)
-available = False
+    flength = len(the_one)
+    available = False
 
-for food in foods:
-    for i in range(len(food.text)):
-        chunk = food.text[i:i+flength].lower()
-        if chunk == the_one:
-            available = True
-
-if available == True:
-    conn = smtplib.SMTP('smtp.gmail.com', 587) # smtp address and port
-    conn.ehlo() # call this to start the connection
-    conn.starttls() # starts tls encryption. When we send our password it will be encrypted.
-    conn.login('youremail@gmail.com, 'appkey')
-    conn.sendmail('youremail@gmail.com', toAddress, 'Subject: Borzaska Alert!\n\nAttention!\n\nYour favourite food is available today!\n\nBon apetite!:\nFood Notifier V1.0')
-    conn.quit()
-    print('Sent notificaton e-mails for the following recipients:\n')
-    for i in range(len(toAddress)):
-        print(toAddress[i])
-    print('')
-else:
+    for food in foods:
+        if the_one in food.text.lower():
+            conn = smtplib.SMTP('smtp.gmail.com', 587)  # smtp address and port
+            conn.ehlo()  # call this to start the connection
+            conn.starttls()  # starts tls encryption. When we send our password it will be encrypted.
+            conn.login('youremail@gmail.com', 'appkey')
+            conn.sendmail('youremail@gmail.com', to_address, email_content)
+            conn.quit()
+            print(f'Sent notification e-mails for the following recipients:\n{to_address}')
+            exit(0)
     print('Your favourite food is not available today.')
